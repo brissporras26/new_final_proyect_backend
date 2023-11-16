a2142cd229ccfd24d3ddb92e59d5bd3e8442e97e
Jorge Nava <jorge.nava@boats.com>
2023-11-16 14:18:30 -0700

    Add new styles

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+require('./public/js/sockets')(io);
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Iniciar el servidor
+server.listen(app.get('port'), () => {
+    console.log('Servidor en el puerto', app.get('port'));
+});
