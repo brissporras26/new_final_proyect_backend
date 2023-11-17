2298c493f021af461fcc287614b877551135918f
Jorge Nava <jorge.nava@boats.com>
2023-11-17 16:37:45 -0700

    reestructured folders

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
 //settings
 app.set('port', process.env.PORT || 3000);
-require('./sockets')(io);
+require('./public/js/sockets')(io);
 
-
-
-console.log();
 // Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Iniciar el servidor
 server.listen(app.get('port'), () => {
-    console.log('Servidor en el puerto', app.get('port'));
+  console.log('Servidor en el puerto', app.get('port'));
 });
