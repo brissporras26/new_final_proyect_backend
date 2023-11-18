c97688d82092cb2169f1071ceb2f719543a9ae31
Jorge Nava <jorge.nava@boats.com>
2023-11-18 14:07:19 -0700

    removed files

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -1,12 +1,12 @@
-module.exports = function functionName(io){
+module.exports = function functionName(io) {
 
-    let nicknames = [];
-    
-    io.on('connection', (socket) => {
-        console.log('New user connected');
-        
-        socket.on('send message', function(data){
-            io.sockets.emit('new message', data);
-        });
+  let nicknames = [];
+
+  io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('send message', function (data) {
+      io.sockets.emit('new message', data);
     });
+  });
 }
