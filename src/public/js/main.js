12e2b58508aed2358901800a3a8aa79c01793f67
Jorge Nava <jorge.nava@boats.com>
2023-11-18 13:46:00 -0700

    Fixed styles, adapted send message event

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,55 +1,50 @@
-$(function (){
-    const socket= io();
+$(function () {
+  const socket = io();
 
-    //obtaining DOM elments form the interface
-    const $messageForm = $('#message-form');
-    const $messageBox = $('#message');
-    const $chat = $('#chat');
+  //obtaining DOM elments form the interface
+  const sendButton = document.getElementById('send-button');
+  const messageInput = document.getElementById('message-input');
+  const $chat = $('#chat');
 
-    //obtaining DOM elments form the nicknameform
+  //obtaining DOM elments form the nicknameform
+  const $nickForm = $('#nickForm');
+  const $nickError = $('#nickError');
+  const $nickname = $('#nickname');
 
-    const $nickForm =  $('#nickForm');
-    const $nickError =  $('#nickError');
-    const $nickname =  $('#nickname');
-    
-    const $users = $('#usernames');
-    
-    $nickForm.submit(e => {
-        e.preventDefault();
-        socket.emit('new user', $nickname.val(), data =>{
-            if(data) {
-                $('#nickWrap').hide();
-                $('#contentWrap').show();
-            }else{
-                $nickError.html('<div class="alert alert-danger">that username already exists</div>');
-            }
-            $nickname.val('');
-        });
+  const $users = $('#usernames');
+
+  // TODO: IMPLEMENT NICK SUMBISSION
+  $nickForm.submit(e => {
+    e.preventDefault();
+    socket.emit('new user', $nickname.val(), data => {
+      if (data) {
+        $('#nickWrap').hide();
+        $('#contentWrap').show();
+      } else {
+        $nickError.html('<div class="alert alert-danger">that username already exists</div>');
+      }
+      $nickname.val('');
     });
+  });
 
-    
-    //events
-    $messageForm.submit( e => {
-        e.preventDefault();
-        socket.emit('send message', $messageBox.val(), data =>{
-            $chat.append(`<pclass="error">${data}</p>`);
-        });
-        $messageBox.val('');
-    });
+  // Events
+  sendButton.addEventListener('click', function () {
+    const message = messageInput.value;
+    socket.emit('send message', message);
+    messageInput.value = '';
+  });
 
+  // Render new message
+  socket.on('new message', function (data) {
+    $chat.append(`<b>${data.nick}</b>: ${data.msg}</br>`);
+  });
 
-    socket.on('new message', function(data) {
-        $chat.append(`<b>${data.nick}</b>: ${data.msg}</br>`);
-    });
-    
-
-    socket.on('usernames', data => {
-        let html = '';
-        for (let i =0; i < data.length; i++){
-            html += `<p>${data[i]}</p>`;
-        }
-        $users.html(html);
-        
-    });
-
+  // Render connected users
+  socket.on('usernames', data => {
+    let html = '';
+    for (let i = 0; i < data.length; i++) {
+      html += `<p>${data[i]}</p>`;
+    }
+    $users.html(html);
+  });
 });
