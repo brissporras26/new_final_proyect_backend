534e46fff356bd8d36f85bff1c0acca20a822163
brissa <porras.acc1@outlook.com>
2023-11-22 22:22:42 -0600

    .

--- public/js/main.js
+++ public/js/main.js
@@ -1,55 +1,56 @@
-$(function (){
-    const socket= io();
+$(function () {
+    const socket = io();
 
-    //obtaining DOM elments form the interface
+    // Obtaining DOM elements from the interface
     const $messageForm = $('#message-form');
     const $messageBox = $('#message');
     const $chat = $('#chat');
 
-    //obtaining DOM elments form the nicknameform
+    // Obtaining DOM elements from the nickname form
+    const $nickForm = $('#nickForm');
+    const $nickError = $('#nickError');
+    const $nickname = $('#nickname');
 
-    const $nickForm =  $('#nickForm');
-    const $nickError =  $('#nickError');
-    const $nickname =  $('#nickname');
-    
     const $users = $('#usernames');
-    
+
     $nickForm.submit(e => {
         e.preventDefault();
-        socket.emit('new user', $nickname.val(), data =>{
-            if(data) {
+        socket.emit('new user', $nickname.val(), data => {
+            if (data) {
                 $('#nickWrap').hide();
                 $('#contentWrap').show();
-            }else{
+            } else {
                 $nickError.html('<div class="alert alert-danger">that username already exists</div>');
             }
             $nickname.val('');
         });
     });
 
-    
-    //events
-    $messageForm.submit( e => {
+    // Events
+    $messageForm.submit(e => {
         e.preventDefault();
-        socket.emit('send message', $messageBox.val(), data =>{
-            $chat.append(`<pclass="error">${data}</p>`);
+        const messageContent = $messageBox.val();
+        const currentTime = new Date();
+        const hours = currentTime.getHours();
+        const minutes = currentTime.getMinutes();
+        const formattedTime = `${hours}:${minutes}`;
+
+        socket.emit('send message', { message: messageContent, time: formattedTime }, data => {
+            $chat.append(`<p class="error">${data}</p>`);
         });
+
         $messageBox.val('');
     });
 
-
-    socket.on('new message', function(data) {
-        $chat.append(`<b>${data.nick}</b>: ${data.msg}</br>`);
+    socket.on('new message', function (data) {
+        $chat.append(`<b>${data.nick}</b> (${data.time}): ${data.msg}</br>`);
     });
-    
 
     socket.on('usernames', data => {
         let html = '';
-        for (let i =0; i < data.length; i++){
+        for (let i = 0; i < data.length; i++) {
             html += `<p>${data[i]}</p>`;
         }
         $users.html(html);
-        
     });
-
 });
--- public/js/sockets.js
+++ public/js/sockets.js
@@ -19,35 +19,45 @@ module.exports = function(io){
             }
         });
 
-        socket.on('send message', (data, cb) =>{
-            
-            var msg = data.trim();
-
-            if(msg.substr(0,3) === '/w'){
-                msg =msg.substr(3);
-                const index = msg.indexOf('');
-                if(index !== -1){
-                    var name =msg.substring(0, index);
-                    var msg = msg.substring(index + 1);
-                    if(name in users){
-                        users[name].emit('whisper',{
-                            msg,
+        socket.on('send message', (data, cb) => {
+            var msg = (data && data.message && typeof data.message === 'string') ? data.message.trim() : '';
+        
+            if (msg.substr(0, 3) === '/w') {
+                msg = msg.substr(3);
+                const index = msg.indexOf(' ');
+                if (index !== -1) {
+                    var name = msg.substring(0, index);
+                    var msgBody = msg.substring(index + 1);
+                    if (name in users) {
+                        users[name].emit('new message', {
+                            msg: msgBody,
                             nick: socket.nickname,
-                            
+                            time: data.time
                         });
-                    }else{
+                    } else {
                         cb('Error, please enter a valid user');
                     }
-
-                }else{
+                } else {
                     cb('Error, please enter your message');
                 }
-            }else{
+            } else {
                 io.emit('new message', {
-                    msg:data,
-                    nick: socket.nickname });
+                    msg: msg,
+                    nick: socket.nickname,
+                    time: data.time
+                });
             }
         });
+
+        socket.on('new message', function(data) {
+            // Añadir clase 'right' si el mensaje es del propio usuario, de lo contrario, usar 'left'
+            const alignmentClass = (data.nick === socket.nickname) ? 'right' : 'left';
+            
+            $chat.append(`<div class="message ${alignmentClass}"><b>${data.nick}</b>: ${data.msg}<span class="time">(${data.time})</span></div>`);
+
+        });
+        
+        
         
             
         socket.on('disconnect', data => {
